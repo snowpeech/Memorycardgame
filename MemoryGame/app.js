b2b5702d06f556f2cd455cf011ff0b0fd4fc16db
Lydia <snowpeech@gmail.com>
2020-03-24 14:53:48 -0500

    memory card game!

---
+++ MemoryGame/app.js
@@ -0,0 +1,124 @@
+const imgSrc = [
+  "/batman",
+  "/beast",
+  "/capt",
+  "/cyclops",
+  "/daredevil",
+  "/hawkeye",
+  "/hulk",
+  "/ironman",
+  "/lantern",
+  "/robin",
+  "/spiderman",
+  "/superman",
+  "/thor"
+];
+const container = document.getElementById("container");
+const scoreDiv = document.getElementById("score");
+const newGamebtn = document.getElementById("newGame");
+
+var gameCounter = 0;
+var score = 0;
+
+newGamebtn.addEventListener("click", function() {
+  location.reload();
+});
+
+// SETTING UP THE BOARD
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+let gameImgs = shuffleArray(imgSrc).slice(0, 8);
+let dblImgs = shuffleArray([...gameImgs, ...gameImgs]);
+
+window.addEventListener("load", drawTable(dblImgs));
+
+function drawTable(imgAry) {
+  let count = 1;
+  for (var i of imgAry) {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("grid-item");
+    newDiv.innerHTML = `<img class="tile hidden" id="${count}" src="./images${i}.png" />`;
+
+    count++;
+    container.appendChild(newDiv);
+  }
+}
+
+// GAMEPLAY
+let pairID = ["x", "x"];
+let pairSrc = "";
+
+function validClick() {
+  gameCounter++;
+  score = Math.floor(gameCounter / 2);
+  scoreDiv.innerHTML = `Score: ${score}`;
+}
+
+function resetSet() {
+  pairID[0] = "x";
+  pairID[1] = "x";
+  pairSrc = "";
+}
+
+container.addEventListener("click", function(e) {
+  if (
+    !e.target.classList.contains("matched") &&
+    e.target.classList.contains("tile") &&
+    !container.classList.contains("animating")
+  ) {
+    //First Click
+    if (pairID[0] === "x") {
+      e.target.classList.toggle("hidden");
+      validClick();
+      // gameCounter++; //gameCounter should be an odd# to set first click
+
+      pairID[0] = e.target.getAttribute("id");
+      pairSrc = e.target.getAttribute("src");
+    }
+
+    //Second Click
+    if (
+      e.target.getAttribute("id") !== pairID[0] &&
+      e.target.getAttribute("id") !== pairID[1]
+    ) {
+      pairID[1] = e.target.getAttribute("id");
+      e.target.classList.toggle("hidden");
+      validClick();
+      // gameCounter++;
+
+      //Tiles Match
+      if (e.target.getAttribute("src") == pairSrc) {
+        e.target.classList.add("matched");
+        let firstMatch = document.getElementById(pairID[0]);
+        firstMatch.classList.add("matched");
+        resetSet();
+        evaluateWin();
+      } else {
+        //Tiles don't match
+        container.classList.add("animating");
+        setTimeout(function() {
+          document.getElementById(pairID[0]).classList.add("hidden");
+          document.getElementById(pairID[1]).classList.add("hidden");
+          resetSet();
+          container.classList.remove("animating");
+        }, 1100);
+      }
+    }
+  }
+});
+
+function evaluateWin() {
+  if (document.querySelectorAll(".hidden").length === 0) {
+    alert(`You Win! Your score is ${score}`);
+    if (score < parseInt(localStorage.getItem("best-score")))
+      localStorage.setItem("best-score", score);
+  }
+}
+
+//would like to break gameplay down into smaller functions and simplyfiy anything with pairID[0] and pairID[1]
---
+++ MemoryGame/scrap.js
@@ -0,0 +1,19 @@
+//where I can't let go of things
+
+
+// function drawTable(imgAry) {
+//   let count = 1;
+//   for (var i of imgAry) {
+//     let newDiv = document.createElement("div");
+//     newDiv.classList.add("grid-item", "hidden");
+//       //the idea doesn't work because if visibility:hidden then you can't click on it :(
+//     let newImage = document.createElement("img");
+//     newImage.src = `./images${i}.png`;
+//     newImage.classList.add("title");
+//     newImage.setAttribute("id", count);
+//     newDiv.appendChild(newImage);
+
+//     count++;
+//     container.appendChild(newDiv);
+//   }
+// }
